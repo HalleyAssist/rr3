36671ac712631ab391faa8a8262655566d55bb8f
Mathew Heard <splitice@users.noreply.github.com>
2018-10-11 16:44:26 +1100

    work on tests

--- spec/rr3_spec.rb
+++ spec/rr3_spec.rb
@@ -3,23 +3,23 @@ require 'spec_helper'
 describe Rr3 do
   before(:all)do
     @tree = Rr3::Tree.new(10)
-    @tree.insert_path "/bar"
-    @tree.insert_path "/zoo", "test"
-    @tree.insert_path "/foo/bar", a: 1
-    @tree.insert_path "/post/{id}", [1]
+    @tree.insert_path 1, "/bar"
+    @tree.insert_path 1, "/zoo", 3
+    @tree.insert_path 1, "/foo/bar", 1
+    @tree.insert_path 1, "/post/{id}", 2
     class A; attr_accessor :bar; end;
-    @tree.insert_path "/user/{id:\\d+}", A.new.tap{|a| a.bar = 1}
+    @tree.insert_path 1, "/user/{id:\\d+}", A.new.tap{|a| a.bar = 1}
     @tree.compile!
   end
 
   it 'matches' do
-    expect(@tree.match("/bar")).to be_nil
-    expect(@tree.match("/zoo")).to eql "test"
-    expect(@tree.match("/foo/bar")).to eql a: 1
-    expect(@tree.match("/post/1234")).to eql [1]
-    expect(@tree.match("/user/1234").bar).to eql 1
-    expect(@tree.match("/user/xxx")).to eql false
-    expect(@tree.match("/asd")).to eql false
+    expect(@tree.match(1, "/bar").data).to be_nil
+    expect(@tree.match(1, "/zoo").data).to eql 3
+    expect(@tree.match(1, "/foo/bar").data).to eql 1
+    expect(@tree.match(1, "/post/1234").data).to eql 2
+    expect(@tree.match(1, "/user/1234").data).to eql 1
+    expect(@tree.match(1, "/user/xxx").data).to eql false
+    expect(@tree.match(1, "/asd").data).to eql false
   end
 
   it 'dumps' do
